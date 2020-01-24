33e333eb4dd80b37d0d541ea29bcbef5c2330a05
Alex Stopa <stopa.alex@gmial.com>
2020-01-24 10:14:13 -0500

    update ::: build

---
+++ precache-manifest.6518f53fc22490d73fa21c1740d61d24.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2e4307235d2270837076d9ed51b3e0d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5874d13bff3eea768382",
+    "url": "/static/css/main.fa1609d1.chunk.css"
+  },
+  {
+    "revision": "5696828e9402ab7080e5",
+    "url": "/static/js/2.618bf35b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.618bf35b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5874d13bff3eea768382",
+    "url": "/static/js/main.8c215f93.chunk.js"
+  },
+  {
+    "revision": "d84a3e19d8a947dff9dd",
+    "url": "/static/js/runtime-main.b2044548.js"
+  }
+]);
